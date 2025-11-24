a7825118c1f495d6d1ccfb3efd43c8e057ff1d08
Halomaster0 <Halomaster2006lol@gmail.com>
2025-11-23 22:34:50 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * Represents a theatrical play.
+ *
+ * @null neither {@code name} nor {@code type} may be {@code null}
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
